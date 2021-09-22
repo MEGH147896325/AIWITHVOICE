07a931a015342c835251e8a472997320324bb8f1
MEGH777 <83692043+MEGH777@users.noreply.github.com>
2021-09-22 09:56:41 +0530

    Updaate

--- javascript/main.js
+++ javascript/main.js
@@ -37,18 +37,21 @@ recognition.onresult = function run (event) {
     
     msg.text = "Opening youtube";
     window.speechSynthesis.speak(msg);
-    }else if(newcontent == "open gmail" || newcontent == "start gmail"){
+    }
+    else if(newcontent == "open gmail" || newcontent == "start gmail"){
       window.open("https://mail.google.com", "_blank");
       msg.text = "Opening gmail";
       window.speechSynthesis.speak(msg);
         
       
-    }else if (newcontent == "start classes"||newcontent == "join classes"){
+    }
+    else if (newcontent == "start classes"||newcontent == "join classes"){
         window.open("https://meet.google.com/yaf-wyxw-wrr", "_blank");
         
         msg.text = "Joining classes";
         window.speechSynthesis.speak(msg);
-    }else if (newcontent.slice(0,6) == "search"){
+    }
+    else if (newcontent.slice(0,6) == "search"){
       search = newcontent.slice(6,newcontent.length);
       
       newsearch = search.replace(/ /g, "+");
@@ -57,41 +60,50 @@ recognition.onresult = function run (event) {
       
       window.open("https://www.google.com/search?q=" + newsearch, "_blank");
 
-    }else if (newcontent == "good"){
+    }
+    else if (newcontent == "good"){
       
       msg.text = "THANK YOU but you should also thank megh for making me";
       window.speechSynthesis.speak(msg);
-    }else if (newcontent == "who made you"){
+    }
+    else if (newcontent == "who made you"){
       msg.text = "M e g h made me he is the smartest boy XD";
       window.speechSynthesis.speak(msg);
-    }else if (newcontent =="show youtube history"){
+    }
+    else if (newcontent =="show youtube history"){
       msg.text = "Showing Youtube history";
       window.speechSynthesis.speak(msg);
       window.open("https://www.youtube.com/feed/history", "_blank");
-    }else if (newcontent == "open youtube dashboard"){
+    }
+    else if (newcontent == "open youtube dashboard"){
       msg.text = "Openiing Your dashboard";
       window.speechSynthesis.speak(msg);
       window.open("https://studio.youtube.com/channel/videos", "_blank");
-      }else if (newcontent.slice(0,5) == "speak")
-      {
+    }
+    else if (newcontent.slice(0,5) == "speak")
+    {
         speak = newcontent.slice(7, newcontent.length);
         msg.text = speak;
         window.speechSynthesis.speak(msg);
-      }else if (newcontent == "open google"){
+    }
+    else if (newcontent == "open google"){
         msg.text = "Openiing google but you can also search by saying , search and your query";
       window.speechSynthesis.speak(msg);
       window.open("https://www.google.com", "_blank");
-      }else if (newcontent == "joke"){
+      }
+      else if (newcontent == "joke"){
         u = 0;
         
         msg.text = item[Math.floor(Math.random()*item.length)];
         
         window.speechSynthesis.speak(msg);
-      }else if (newcontent == "date"){
+      }
+      else if (newcontent == "date"){
         today = mm + '/' + dd + '/' + yyyy;
         msg.text = today;
         window.speechSynthesis.speak(msg);
-      }else if (newcontent.slice(0,14) == "youtube search"){
+      }
+      else if (newcontent.slice(0,14) == "youtube search"){
         search = newcontent.slice(14,newcontent.length);
         
         newsearch = search.replace(/ /g, "+");
@@ -100,6 +112,12 @@ recognition.onresult = function run (event) {
         
         window.open("https://www.youtube.com/results?search_query=" + newsearch, "_blank");
   
+      }
+      else if (newcontent == "open discord"||newcontent == "start discord"){
+        msg.text = "opening discord";
+        window.speechSynthesis.speak(msg);
+        
+        window.open("https://discord.com/channels/@me", "_blank");
       }
       
   document.getElementById("caught").innerHTML = content;
